50aeb6b2b7515e8a8283da1de327a88e549058de
Lucas Jones <Necxii@gmail.com>
2014-04-01 12:25:24 +0100

    Basic project compiling/working

--- src/Playground.hx
+++ src/Playground.hx
@@ -1,21 +1,19 @@
 package ;
 
-import milkshake.Milkshake;
-
 @:expose
 class Playground
 {
 	function new()
 	{
 		#if js
-		new Milkshake().boot(500, 500);
+		new PlaygroundGame().boot(500, 500);
 		#end
 	}
 	
 	public static function main()
 	{
 		#if cs
-		new Milkshake().boot(500, 500);
+		new PlaygroundGame().boot(500, 500);
 		#end
 	}
 }
--- src/PlaygroundGame.hx
+++ src/PlaygroundGame.hx
@@ -1,10 +1,31 @@
-package Milkshake;
+package ;
+
+import milkshake.Milkshake;
+import milkshake.core.GameObject;
+import milkshake.core.Sprite;
+import pixi.Stage;
 
 class PlaygroundGame extends Milkshake
 {
+	//var stage:GameObject;
+	
 	public function new() 
 	{
 		super();
+		
+		
+	}
+	
+	override public function setStage(stage:Stage):Void 
+	{
+		super.setStage(stage);
+		
+		var bunny:Sprite = new Sprite("bunny.png");
+		var bunnyContainer:GameObject = new GameObject();
+		
+		bunnyContainer.addNode(bunny);
+		
+		stage.addChild(bunny.sprite);
 	}
 	
 	override public function update(delta:Float):Void 
