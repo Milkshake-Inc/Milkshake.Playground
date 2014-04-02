958e502b3645a7d2262bf072585affed44273a96
Lucas Jones <Necxii@gmail.com>
2014-04-02 13:23:48 +0100

    Added sprite/go example

--- src/PlaygroundGame.hx
+++ src/PlaygroundGame.hx
@@ -7,29 +7,35 @@ import pixi.Stage;
 
 class PlaygroundGame extends Milkshake
 {
-	//var stage:GameObject;
+	var bunny:Sprite;
 	
 	public function new() 
 	{
-		super();
-		
-		
+		super();		
 	}
 	
 	override public function setStage(stage:Stage):Void 
 	{
 		super.setStage(stage);
 		
-		var bunny:Sprite = new Sprite("bunny.png");
-		var bunnyContainer:GameObject = new GameObject();
+		var gameObject:GameObject = new GameObject();
+		bunny = new Sprite("bunny.png");
+		bunny.scaleX = bunny.scaleY = 2;
+		bunny.pivotX = 80;
+		bunny.pivotY = 80;
+		bunny.x = 250;
+		bunny.y = 250;
+		gameObject.addNode(bunny);
 		
-		bunnyContainer.addNode(bunny);
-		
-		stage.addChild(bunny.sprite);
+		stage.addChild(gameObject.displayObject);
 	}
 	
 	override public function update(delta:Float):Void 
 	{
 		super.update(delta);
+		
+		//bunny.rotation += 0.05;
+		bunny.scaleX = Math.sin(delta / 400) * 2;
+		//bunny.scaleY = Math.cos(delta / 100) * 2;
 	}
 }
