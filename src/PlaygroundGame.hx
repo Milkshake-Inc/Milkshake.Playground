b7386d8328e87b288ae3cf8e52f1382523d1b5f8
Lucas Jones <Necxii@gmail.com>
2014-04-08 11:54:39 +0100

    Update

--- src/PlaygroundGame.hx
+++ src/PlaygroundGame.hx
@@ -1,9 +1,30 @@
 package ;
 
 import milkshake.game.MilkshakeGame;
+import milkshake.game.scene.component.SceneComponent;
 import milkshake.game.scene.Scene;
 import milkshake.core.GameObject;
+import milkshake.core.Node;
 import milkshake.core.Sprite;
+
+class ExampleSceneComponent extends SceneComponent
+{
+	public function new()
+	{
+		super("ExampleSceneComponent");
+	}
+	
+	override private function nodeAdded(node:Node):Void 
+	{
+		if (Std.is(node, Sprite))
+		{
+			var sprite:Sprite = cast node;
+			
+			sprite.scaleX = 2;
+			sprite.scaleY = 2;
+		}
+	}
+}
 
 class PlaygroundGame extends MilkshakeGame
 {
@@ -14,17 +35,20 @@ class PlaygroundGame extends MilkshakeGame
 		super();
 		
 		var scene:Scene = new Scene();
+		scene.componentManager.addNode(new ExampleSceneComponent());
+		
 		
 		var gameObject:GameObject = new GameObject();
+		scene.addNode(gameObject);
+		
 		bunny = new Sprite("bunny.png");
-		bunny.scaleX = bunny.scaleY = 2;
 		bunny.pivotX = 80;
 		bunny.pivotY = 80;
 		bunny.x = 250;
 		bunny.y = 250;
 		gameObject.addNode(bunny);
 		
-		scene.addNode(gameObject);
+		
 		
 		sceneManager.addScene("defaultScene", scene);
 	}
@@ -35,7 +59,7 @@ class PlaygroundGame extends MilkshakeGame
 		super.update(delta);
 		
 		//bunny.rotation += 0.05;
-		bunny.scaleX = Math.sin(delta / 400) * 2;
+		//bunny.scaleX = Math.sin(delta / 400) * 2;
 		//bunny.scaleY = Math.cos(delta / 100) * 2;
 	}
 }
